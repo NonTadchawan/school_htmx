a054ece06c103b418a38d77cc3d2232b67562fd3
non.tadchawan <tadchawan@geniustree.co.th>
2024-05-28 13:11:18 +0700

    add modal

--- src/main/java/org/acme/controller/StudentController.java
+++ src/main/java/org/acme/controller/StudentController.java
@@ -4,6 +4,7 @@ import io.quarkiverse.renarde.htmx.HxController;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.acme.repository.StudentRepo;
 
@@ -11,12 +12,22 @@ import org.acme.repository.StudentRepo;
 public class StudentController extends HxController {
     @Inject
     StudentRepo studentRepo;
+
     @CheckedTemplate
     public static class Templates {
         public static native TemplateInstance student();
+
+        public static native TemplateInstance modal();
     }
+
     @Path("/")
-    public TemplateInstance student(){
+    public TemplateInstance student() {
         return Templates.student();
+    }
+
+    @GET
+    @Path("/modal")
+    public TemplateInstance modal() {
+        return Templates.modal();
     }
 }
