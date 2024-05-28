0e9d961826e6e9f151a88f4e0d62cd56be956be0
non.tadchawan <tadchawan@geniustree.co.th>
2024-05-28 16:59:50 +0700

    update

--- src/main/java/org/acme/controller/StudentController.java
+++ src/main/java/org/acme/controller/StudentController.java
@@ -4,8 +4,9 @@ import io.quarkiverse.renarde.htmx.HxController;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 import org.acme.model.Student;
 import org.acme.repository.StudentRepo;
 import org.jboss.resteasy.reactive.RestForm;
@@ -43,10 +44,6 @@ public class StudentController extends HxController {
         student.setDegree(degree);
         studentRepo.persist(student);
     }
-/*    @POST
-    @Path("/")
-    public void saveStudent(Student student) {
-        studentRepo.persist(student);
-    }*/
+
 }
 
