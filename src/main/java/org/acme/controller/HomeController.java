15647b9be11592153836911a479956340afe59a4
non.tadchawan <tadchawan@geniustree.co.th>
2024-05-28 14:37:47 +0700

    add pom

--- src/main/java/org/acme/controller/HomeController.java
+++ src/main/java/org/acme/controller/HomeController.java
@@ -5,15 +5,15 @@ import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 
 @Path("/")
 public class HomeController extends HxController {
     @CheckedTemplate
     public static class Templates {
         public static native TemplateInstance home();
+
         public static native TemplateInstance about();
+
         public static native TemplateInstance contact();
     }
 
@@ -21,7 +21,6 @@ public class HomeController extends HxController {
     public TemplateInstance home() {
         return Templates.home();
     }
-
 
     public TemplateInstance about() {
         return Templates.about();
--- src/main/java/org/acme/controller/StudentController.java
+++ src/main/java/org/acme/controller/StudentController.java
@@ -5,7 +5,9 @@ import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import org.acme.model.Student;
 import org.acme.repository.StudentRepo;
 
 @Path("/student")
@@ -29,5 +31,10 @@ public class StudentController extends HxController {
     @Path("/modal")
     public TemplateInstance modal() {
         return Templates.modal();
+    }
+
+    @POST
+    public void saveStudent(Student student){
+        studentRepo.saveStudent(student);
     }
 }
