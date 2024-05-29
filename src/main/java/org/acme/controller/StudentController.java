c71f4286e48096e903f1aa9b7a9ca910c8f9db99
non.tadchawan <tadchawan@geniustree.co.th>
2024-05-29 10:05:37 +0700

    update student page

--- src/main/java/org/acme/controller/StudentController.java
+++ src/main/java/org/acme/controller/StudentController.java
@@ -3,6 +3,7 @@ package org.acme.controller;
 import io.quarkiverse.renarde.htmx.HxController;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
+import io.smallrye.common.annotation.Blocking;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -11,6 +12,8 @@ import org.acme.model.Student;
 import org.acme.repository.StudentRepo;
 import org.jboss.resteasy.reactive.RestForm;
 
+import java.util.List;
+
 @Path("/student")
 public class StudentController extends HxController {
     @Inject
@@ -18,14 +21,18 @@ public class StudentController extends HxController {
 
     @CheckedTemplate
     public static class Templates {
-        public static native TemplateInstance student();
-
+        public static native TemplateInstance student(List<Student> students);
+        public static native TemplateInstance student$list(List<Student> students);
         public static native TemplateInstance modal();
     }
 
     @Path("/")
+    @Blocking
     public TemplateInstance student() {
-        return Templates.student();
+        if(isHxRequest()){
+            return Templates.student$list(studentRepo.listAll());
+        }
+        return Templates.student(studentRepo.listAll());
     }
 
     @GET
